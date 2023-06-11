316b20763b12acf4c75f4f8762ad667c7e92dc1a
manolast <manuel.stapff@gmail.com>
2023-06-10 21:05:52 -0700

    terminado

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
-const {encode, decode} = require('gpt-3-encoder');
+const {encode} = require('gpt-3-encoder');
 const axios = require("axios");
 const openaiApiKey = process.env.OPEN_AI_API_KEY;
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET
-  });
+});
+
+let botId;
+let appId;
+(async () => {
+    try {
+      const authTestResponse = await app.client.auth.test({
+        token: process.env.SLACK_BOT_TOKEN,
+      });
+      botId = authTestResponse.bot_id;
+      appId = authTestResponse.user_id;
+      console.log('Bot ID:', botId);
+      console.log('App ID:', appId);
+  
+      await app.start(process.env.PORT || 3000);
+      console.log('Bolt app is running!');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+})();
 
 const promptSinVariables = `Instructions: Generate a TLDR (summary) of the conversation based on the provided messages and previous TLDR.
 
@@ -18,37 +37,19 @@ TLDR:
 
 Messages:
 
-
+####
 Summary:`
 const tokensSinVariables = encode(promptSinVariables).length;
 
-let botId;
-let appId;
-(async () => {
-    try {
-        const authTestResponse = await app.client.auth.test({
-            token: process.env.SLACK_BOT_TOKEN,
-        });
-        botId = authTestResponse.bot_id;
-        appId = authTestResponse.user_id;
-        console.log('Bot ID:', botId);
-        console.log('App ID:', appId);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-  })();
-
 let mensajes = "";
-let ultimoTLDR = "No hay ultimo contexto.";
-
-(async () => {
-    await app.start(process.env.PORT || 3000);
-    console.log('Bolt app is running!');
-})();
+let ultimoTLDR = "There is no last context.";
 
 app.message(async ({ message, say }) => {
     const channelId = message.channel;
-    if(isBotInChannel(channelId)){
+
+    //checkeamos si no se arrobo al bot para dejar de lado la funcion de abajo y que se ejecute el mention
+    //tambien checkeamos que el bot este integrado en el canal con isBotInChannel
+    if(isBotInChannel(channelId) && !message.text.includes(`@${botId}`) && !message.text.includes(`@${appId}`)){
         const userInfo = await app.client.users.info({
             token: process.env.SLACK_BOT_TOKEN,
             user: message.user
@@ -58,16 +59,13 @@ app.message(async ({ message, say }) => {
         const content = message.text;
         const lastMessage = `${userName}: ${content}\n`;
         //sumo los tokens de todos los strings, si se pasa del limite (dejamos un margen por las dudas) se genera el resumen y se envia
-        if (encode(mensajes).length + encode(lastMessage).length + encode(ultimoTLDR).length + tokensSinVariables > 3500) {
+        if(encode(mensajes).length + encode(lastMessage).length + encode(ultimoTLDR).length + tokensSinVariables > 3500){
             await generarYMandarResumen(ultimoTLDR, mensajes, say);
             mensajes = lastMessage;
         } else {
             mensajes += lastMessage;
         }
-    }else{
-        console.log("no esta la integracion");
     }
-
   });
 
 app.event('app_mention', async ({ event, say }) => {
@@ -77,7 +75,7 @@ app.event('app_mention', async ({ event, say }) => {
 async function generarYMandarResumen(context, messages, say) {
     try {
         const summary = await generateTLDR(context, messages);
-        await say(`Resumen: ${summary}`);
+        await say(`Summary: ${summary}`);
         //se asgina mensajes vacio por default. Si se genero y envio el mensaje por tope de tokens, se va a asignar de otra manera alli
         mensajes = "";
         ultimoTLDR = summary;
@@ -99,8 +97,10 @@ async function generateTLDR(context, messages) {
         
             Messages:
             ${messages}
-        
-            Summary:`,
+
+            ####
+            Summary:
+            `,
             model: "text-davinci-003",
             max_tokens: 4000,
             temperature: 0.1, 
